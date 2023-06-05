13bdd8505abcac8f420a4f121ec9b9f92ac88853
Anubhab <anubhabpal1@gmail.com>
2023-06-05 21:34:46 +0530

    App.js ko modify karaya hai

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -11,13 +11,18 @@ class App extends Component {
   }
 
   increment = () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState({ count: this.state.count + 1 });
   }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
   render() {
     return (
       <div className="App">
-        <button className="counter">+</button>
-        <button className="counter">-</button>
+        <button onClick={this.increment} className="counter">+</button>
+        <button onClick={this.decrement} className="counter">-</button>
         <h2>{this.state.count}</h2>
       </div>
     );
