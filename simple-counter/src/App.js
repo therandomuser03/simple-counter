9a1d87423f7b6c53b53acdf2d2f4f01d391751c5
Anubhab <anubhabpal1@gmail.com>
2023-06-05 21:26:31 +0530

    updated App.js

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,25 +1,27 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  render() {
+    return (
+      <div className="App">
+        <button className="counter">+</button>
+        <button className="counter">-</button>
+        <h2>{this.state.count}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
